65e1446c1252f9c3caf18c9995b6271566d801b9
Alonso Cortes <alocortes.dev@gmail.com>
2022-04-26 20:17:33 -0600

    Initial commit

---
+++ src/main/java/com/cenfotec/PrjGraphQL/mutation/VehicleMutation.java
@@ -0,0 +1,18 @@
+package com.cenfotec.PrjGraphQL.mutation;
+
+import com.cenfotec.PrjGraphQL.entities.Vehicle;
+import com.cenfotec.PrjGraphQL.services.VehicleService;
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class VehicleMutation implements GraphQLMutationResolver {
+
+    @Autowired
+    private VehicleService vehicleService;
+
+    public Vehicle createVehicle(String type, String modelCode, String brandName, String launchDate) {
+        return this.vehicleService.createVehicle(type, modelCode, brandName, launchDate);
+    }
+}
